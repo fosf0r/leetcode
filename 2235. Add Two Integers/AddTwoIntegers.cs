580c5678e4da8e12dddddb890b20d57f804f8913
fosf0r <fosf0r@outlook.com>
2023-02-26 12:12:26 -0500

    Ransom note: Removed benchmark stuff; Added leetcode problem 2235.

---
+++ 2235. Add Two Integers/AddTwoIntegers.cs
@@ -0,0 +1,9 @@
+﻿// Test case:
+int num1 = 12;
+int num2 = 5;
+
+Console.WriteLine(Sum(num1, num2)); // should return 17
+
+int Sum(int num1, int num2) {
+    return num1 + num2; // duh
+}
--- 383. Ransom Note/RansomNote.cs
+++ 383. Ransom Note/RansomNote.cs
@@ -1,39 +1,30 @@
 ﻿
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
 using System.Collections;
 
-[MemoryDiagnoser]
-public class Program {
+string ransomNote = "aa";
+string magazine1 = "aba";
+string magazine2 = "bbb";
+Console.WriteLine(CanConstruct(ransomNote, magazine1)); // should return True
+Console.WriteLine(CanConstruct(ransomNote, magazine2)); // should return False
 
-    public IEnumerable<string> testCases = new string[] { "aa", "aba" };;
-
-    [Benchmark]
-    [ParamsSource(nameof(testCases))]
-    public static bool CanConstruct(string ransomNote, string magazine) {
-        var SkipTable = new Hashtable();
-        for (int i = 0; i < ransomNote.Length; i++) {
-            bool foundthisone = false;
-            for (int j = 0; j < magazine.Length; j++) {
-                if (SkipTable.ContainsKey(j)) {
-                    continue;
-                } else {
-                    if (ransomNote[i] == magazine[j]) {
-                        foundthisone = true;
-                        SkipTable.Add(j, magazine[j]);
-                        break;
-                    }
+bool CanConstruct(string ransomNote, string magazine) {
+    var SkipTable = new Hashtable();
+    for (int i = 0; i < ransomNote.Length; i++) {
+        bool foundthisone = false;
+        for (int j = 0; j < magazine.Length; j++) {
+            if (SkipTable.ContainsKey(j)) {
+                continue;
+            } else {
+                if (ransomNote[i] == magazine[j]) {
+                    foundthisone = true;
+                    SkipTable.Add(j, magazine[j]);
+                    break;
                 }
             }
-            if (!foundthisone) {
-                return false;
-            }
         }
-        return true;
+        if (!foundthisone) {
+            return false;
+        }
     }
-
-    static void Main() {
-        Console.WriteLine(BenchmarkRunner.Run<Program>);
-    }
-
+    return true;
 }
