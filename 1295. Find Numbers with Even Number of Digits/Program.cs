25069026b6098bba4a1d5239ed88fd1e5ffa8153
fosf0r <fosf0r@outlook.com>
2023-02-26 11:48:58 -0500

    Updated SLN for the folder renames and solved leetcode number 1295

---
+++ 1295. Find Numbers with Even Number of Digits/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] mynums = new int[] { 12,345,2,6,7896 };
+
+Console.WriteLine(FindNumbers(mynums)); // should be 2
+
+int FindNumbers(int[] nums) {
+    int counter = 0;
+    for (int i = 0; i < nums.Length; i++) {
+        if (nums[i].ToString().Length % 2 == 0) {
+            counter++;
+        }
+    }
+    return counter;
+}
