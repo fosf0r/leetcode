98d3a1fe7c3251013a95ae9f3dd61e01d29244ec
fosf0r <fosf0r@outlook.com>
2023-02-26 12:02:28 -0500

    Added leetcode problems 167 and 1672

---
+++ 167. Two Sum II - Input Array Is Sorted/TwoSumII.cs
@@ -0,0 +1,27 @@
+﻿// Test case:
+int[] mynums = new int[] { 2, 7, 11, 15 };
+int target = 9;
+foreach (var item in TwoSum2(mynums, target)) {
+    Console.WriteLine(item); // test case should return: 1, then 2
+}
+
+
+int[] TwoSum2(int[] nums, int target) {
+    int left = 0, right = nums.Length - 1;
+    while ((nums[left] + nums[right]) != target)
+        if ((nums[left] + nums[right]) > target) {
+            if (nums[right - 1] == nums[right]) {
+                for (int i = right; right > left; i--) {
+                    if (nums[i] != nums[i - 1]) {
+                        right = i;
+                        break;
+                    }
+                }
+            } else {
+                right--;
+            }
+        } else {
+            left++;
+        }
+    return new int[] { left + 1, right + 1 };
+}
---
+++ 1672. Richest Customer Wealth/RichestCustomerWealth.cs
@@ -0,0 +1,19 @@
+﻿// Test case:
+int[][] accounts = new int[][] { new int[] { 1, 2, 3 } , new int[] { 3, 1, 1 } };
+
+Console.WriteLine(MaximumWealth(accounts)); // should return 6
+
+int MaximumWealth(int[][] accounts) {
+    int wealth;
+    int richest = 0;
+    for (int i = 0; i < accounts.Length; i++) {
+        wealth = 0;
+        for (int j = 0; j < accounts[i].Length; j++) {
+            wealth += accounts[i][j];
+            if (wealth > richest) {
+                richest = wealth;
+            }
+        }
+    }
+    return richest;
+}
