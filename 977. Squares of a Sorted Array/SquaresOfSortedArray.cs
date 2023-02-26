be13f3164a9ce8983ea8fa695fc1c89af798d867
fosf0r <fosf0r@outlook.com>
2023-02-26 14:15:28 -0500

    Solved leetcode problems 977 and 1089. 1089 very naively.

---
+++ 977. Squares of a Sorted Array/SquaresOfSortedArray.cs
@@ -0,0 +1,13 @@
+﻿// Test case:
+// Input: nums = [-4, -1, 0, 3, 10]
+// Output: [0,1,9,16,100]
+
+int[] SortedSquares(int[] nums) {
+    // Square them all in place:
+    for (int i = 0; i < nums.Length; i++) {
+        nums[i] *= nums[i];
+    }
+    // Naive in-place sort:
+    Array.Sort(nums);
+    return nums;
+}
