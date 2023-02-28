5d05604fd0c5347514ebdb72bec05880910e46ee
fosf0r <fosf0r@outlook.com>
2023-02-27 22:46:28 -0500

    Failed at leetcode 20, updated leetcode 4

---
+++ 20. Valid Parentheses/ValidParentheses.cs
@@ -0,0 +1,59 @@
+﻿// Test case:
+
+Console.WriteLine(IsValid("()")); // should return true
+Console.WriteLine(IsValid("()[]{}")); // should return true
+Console.WriteLine(IsValid("(]")); // should return false
+Console.WriteLine(IsValid("{{")); // should return false
+
+
+bool IsValid(string s)
+{
+    bool curlyopen = false;
+    bool curlyclosed = false;
+    bool parensopen = false;
+    bool parensclosed = false;
+    bool squareopen = false;
+    bool squareclosed = false;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        switch (s[i].ToString())
+        {
+            case "{":
+            {
+                curlyopen = true;
+                break;
+            }
+            case "}":
+            {
+                curlyclosed = true;
+                break;
+            }
+            case "[":
+            {
+                squareopen = true;
+                break;
+            }
+            case "]":
+            {
+                squareclosed = true;
+                break;
+            }
+            case "(":
+            {
+                parensopen = true;
+                break;
+            }
+            case ")":
+            {
+                parensclosed = true;
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    return !(curlyopen != curlyclosed || parensopen != parensclosed || squareopen != squareclosed);
+}
+
--- 4. Median of Two Sorted Arrays/MedianOfTwoSortedArrays.cs
+++ 4. Median of Two Sorted Arrays/MedianOfTwoSortedArrays.cs
@@ -1,4 +1,5 @@
 ﻿// test case:
+
 int[] nums1a = new int[] { 1, 3 };
 int[] nums2a = new int[] { 2 };
 Console.WriteLine(FindMedianSortedArrays(nums1a, nums2a)); // should return 2.0
@@ -8,23 +9,47 @@ int[] nums1b = new int[] { 1, 2 };
 int[] nums2b = new int[] { 3, 4 };
 Console.WriteLine(FindMedianSortedArrays(nums1b, nums2b)); // should return 2.5
 
+// this one's easier to read but got a lower score at leetcode:
+/*double FindMedianSortedArrays(int[] nums1, int[] nums2)
+{
+    double answer = 0;
+    int[] merged = nums1.Concat(nums2).OrderBy(x => x).ToArray();
 
-double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+    if (merged.Length % 2 == 0)
+    {
+        int left = (merged.Length - 1) / 2;
+        answer = (double)(merged[left] + merged[left + 1]) / 2;
+    } else
+    {
+        answer = merged[merged.Length - 1 - (merged.Length / 2)];
+    }
+
+    return answer;
+}
+*/
+
+double FindMedianSortedArrays(int[] nums1, int[] nums2)
+{
     double answer = 0;
     List<int> merged = new();
-    for (int i = 0; i < nums1.Length; i++) {
+
+    for (int i = 0; i < nums1.Length; i++)
+    {
         merged.Add(nums1[i]);
     }
-    for (int i = 0; i < nums2.Length; i++) {
+    for (int i = 0; i < nums2.Length; i++)
+    {
         merged.Add(nums2[i]);
     }
-    
+
     merged.Sort(); // naive sort in place
 
-    if (merged.Count % 2 == 0) {
+    if (merged.Count % 2 == 0)
+    {
         int left = (merged.Count - 1) / 2;
         answer = (double)(merged[left] + merged[left + 1]) / 2;
-    } else {
+    } else
+    {
         answer = merged[merged.Count - 1 - (merged.Count / 2)];
     }
 
